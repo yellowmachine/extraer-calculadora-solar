7a183c2abd7c5b72303d3cc226e17403ef03fb54
yellow <yellow.machine@mailfence.com>
2022-08-16 06:20:26 +0200

    added openpyxl

---
+++ main.py
@@ -0,0 +1,14 @@
+from openpyxl import load_workbook
+
+wb2 = load_workbook('abc.xlsx')
+#wb2 = load_workbook('Hoja simplificada calculo anual.xlsx')
+print(wb2.sheetnames)
+
+ws = wb2['Hoja1']
+c1 = ws['A1']
+c2 = ws['A2']
+
+print(c1.value)
+print(c2.value)
+
+#print(dir(c1))
