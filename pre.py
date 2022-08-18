9276928e14536ee845a31907e1e7082e96f7f53d
yellow <yellow.machine@mailfence.com>
2022-08-18 10:20:18 +0200

    replace to create var names

--- pre.py
+++ pre.py
@@ -4,6 +4,32 @@ from dependencies import dependencies
 
 values_f = {}
 
+def _replace(txt):
+    txt = txt.replace("!", '-')
+    txt = txt.replace("ñ", 'ny')
+    txt = txt.replace("Ñ", 'Ny')
+    txt = txt.replace("á", 'a')
+    txt = txt.replace("é", 'e')
+    txt = txt.replace("í", 'i')
+    txt = txt.replace("ó", 'o')
+    txt = txt.replace("ú", 'u')
+    txt = txt.replace("Á", 'A')
+    txt = txt.replace("É", 'E')
+    txt = txt.replace("Í", 'I')
+    txt = txt.replace("Ó", 'O')
+    txt = txt.replace("Ú", 'U')
+    txt = txt.replace(" ", '_')
+
+    return txt
+
+def replace(o):
+    if o.__class__ == dict:
+        return {k: _replace(v) for k, v in o.items()}
+    elif o is not None:
+        return _replace(o)
+    else:
+        return None
+
 def build(nodes, wb):
     ret = {}
 
@@ -26,5 +52,5 @@ x = tuple(ts.static_order())
 #print(x)
 
 for k in x:
-    print(f'{k};{values_f[k][0]};{values_f[k][1]};{values_f[k][2]}')
+    print(f';{replace(k)};{values_f[k][0]};{replace(values_f[k][1])};{values_f[k][2]}')
     
